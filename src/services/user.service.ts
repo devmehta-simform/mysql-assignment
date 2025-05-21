7e00863ca4efb52b4c8ee0d74bb2e47994845d24
devmehta-simform <dev.mehta@simformsolutions.com>
2025-05-21 09:34:20 +0530

    feat: handle query to get order details for user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,8 @@
 import { User } from '../models/user';
 import { type RequestHandler } from 'express';
+import { sequelize } from '../utils/sequelizeProvider';
+import { Product } from '../models/product';
+import { Order } from '../models/order';
 
 export const getAllUsers: RequestHandler = async (req, res) => {
   const users = await User.findAll({ where: { deleted_at: null } });
@@ -41,4 +44,39 @@ export const deleteUserById: RequestHandler = async (req, res) => {
   const userId = req.params['id'];
   await User.update({ deleted_at: new Date() }, { where: { id: userId } });
   res.status(204).json();
+};
+
+export const getOrdersOfUser: RequestHandler = async (req, res) => {
+  const userId = req.params['id'];
+  const orders = await User.findAll({
+    where: { deleted_at: null, id: userId },
+    attributes: ['email', 'name', 'address'],
+    include: [
+      {
+        model: Order,
+        attributes: [
+          'status',
+          'id',
+          'order_date',
+          'expected_delivery_date',
+          [
+            sequelize.literal('case when Orders.status="pending" then DATEDIFF(Orders.expected_delivery_date,now()) else 0 end'),
+            'days_left_before_delivery',
+          ],
+        ],
+        where: { deleted_at: null },
+        include: [
+          {
+            model: Product,
+            attributes: ['id', 'name', 'description', 'price'],
+            where: { deleted_at: null },
+            through: {
+              attributes: [],
+            },
+          },
+        ],
+      },
+    ],
+  });
+  res.status(200).json(orders);
 };
